ddd4ced1d1da3242c74dc9857721748454eea995
VisualDevelopment <drherocrafterslab@gmail.com>
2019-12-20 21:27:45 +0200

    Now using String Interpolation

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace Bootstrap
 {
     class Program
     {
-        static readonly string[] projects = { "SparkBoot", "SparkKernel" };
+        static readonly string[] projects = { "SparkKernel", "SparkKernel" };
         static readonly Dictionary<string, string> files = new Dictionary<string, string>()
         {
             { "/EFI/BOOT/BOOTX64.EFI", "SparkBoot/BOOTX64.EFI" },
@@ -23,7 +23,7 @@ namespace Bootstrap
             Directory.SetCurrentDirectory("../../../");
             Console.WriteLine(Directory.GetCurrentDirectory());
             string target = args.Length > 0 ? args[0] : "x86_64-Debug";
-            DirectoryInfo target_dir = new DirectoryInfo(string.Format("out/{0}", target));
+            DirectoryInfo target_dir = new DirectoryInfo($"out/{target}");
 
             Console.WriteLine("Welcome to the Spark OS Bootstraper");
 
@@ -41,7 +41,7 @@ namespace Bootstrap
             Console.WriteLine("==> Making directories...");
             Console.ResetColor();
 
-            Directory.CreateDirectory(string.Format("out/{0}/system", target));
+            Directory.CreateDirectory($"out/{target}/system");
 
             Console.ForegroundColor = ConsoleColor.Magenta;
 
@@ -50,15 +50,15 @@ namespace Bootstrap
 
             foreach (string project_path in projects)
             {
-                DirectoryInfo project = new DirectoryInfo(string.Format("out/{0}/{1}", target, project_path));
+                DirectoryInfo project = new DirectoryInfo($"out/{target}/{project_path}");
                 if (!project.Exists)
-                    Directory.CreateDirectory(string.Format("out/{0}/{1}", target, project_path));
+                    Directory.CreateDirectory($"out/{target}/{project_path}");
 
-                if (!Directory.Exists(project_path) || !File.Exists(string.Format("{0}/CMakeLists.txt", project_path)))
+                if (!Directory.Exists(project_path) || !File.Exists($"{project_path}/CMakeLists.txt"))
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
 
-                    Console.WriteLine(string.Format("CMake project '{0}' is missing CMakeLists.txt or does not exist. <==", project_path));
+                    Console.WriteLine($"CMake project '{project_path}' is missing CMakeLists.txt or does not exist. <==");
                     Console.ResetColor();
                     return 1;
                 }
@@ -69,11 +69,11 @@ namespace Bootstrap
                 foreach (DirectoryInfo d in project.GetDirectories())
                     d.Delete(true);
 
-                Directory.SetCurrentDirectory(string.Format("out/{0}/{1}", target, project_path));
+                Directory.SetCurrentDirectory($"out/{target}/{project_path}");
 
                 try
                 {
-                    ProcessStartInfo cmake = new ProcessStartInfo("cmake.exe", string.Format("-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_NASM_COMPILER=nasm -G \"Ninja\" ../../../{0}", project_path));
+                    ProcessStartInfo cmake = new ProcessStartInfo("cmake.exe", $"-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_NASM_COMPILER=nasm -G \"Ninja\" ../../../{project_path}");
                     Process cmake_p = Process.Start(cmake);
 
                     cmake_p.WaitForExit();
@@ -82,7 +82,7 @@ namespace Bootstrap
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
 
-                        Console.WriteLine(string.Format("CMake exited with exit code {0}", cmake_p.ExitCode));
+                        Console.WriteLine($"CMake exited with exit code {cmake_p.ExitCode}");
                         Console.ResetColor();
 
                         return 1;
@@ -98,7 +98,7 @@ namespace Bootstrap
                     {
                         Console.ForegroundColor = ConsoleColor.Red;
 
-                        Console.WriteLine(string.Format("Ninja exited with exit code {0}", ninja_p.ExitCode));
+                        Console.WriteLine($"Ninja exited with exit code {ninja_p.ExitCode}");
                         Console.ResetColor();
 
                         return 1;
@@ -109,7 +109,7 @@ namespace Bootstrap
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
 
-                    Console.WriteLine(string.Format("An error occurred:\n\r{0}", exc.Message));
+                    Console.WriteLine($"An error occurred:\n\r{exc.Message}");
                     Console.ResetColor();
 
                     return 1;
@@ -125,7 +125,7 @@ namespace Bootstrap
 
             SetupHelper.SetupComplete();
 
-            using Stream img = File.Create(string.Format("out/{0}/spark.img", target));
+            using Stream img = File.Create($"out/{target}/spark.img");
 
             Disk disk = Disk.Initialize(img, DiscUtils.Streams.Ownership.None, 30 * 1024 * 1024);
             GuidPartitionTable.Initialize(disk, WellKnownPartitionType.WindowsFat);
@@ -133,7 +133,7 @@ namespace Bootstrap
             using FatFileSystem fs = FatFileSystem.FormatPartition(disk, 0, string.Empty);
 
             foreach (KeyValuePair<string, string> item in files)
-                fs.CopyFile(string.Format("out/{0}/{1}", target, item.Value), item.Key, true);
+                fs.CopyFile($"out/{target}/{item.Value}", item.Key, true);
 
             Console.ForegroundColor = ConsoleColor.Green;
 
