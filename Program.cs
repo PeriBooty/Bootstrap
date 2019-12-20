d96013b8c40658ce5cbfa918de8da5c577bc0907
VisualDevelopment <drherocrafterslab@gmail.com>
2019-12-20 21:09:39 +0200

    Updated gitignore, made program usable

--- Program.cs
+++ Program.cs
@@ -11,15 +11,17 @@ namespace Bootstrap
 {
     class Program
     {
-        static readonly string[] projects = { "SparkKernel" };
+        static readonly string[] projects = { "SparkBoot", "SparkKernel" };
         static readonly Dictionary<string, string> files = new Dictionary<string, string>()
         {
-            { "", "" },
+            { "/EFI/BOOT/BOOTX64.EFI", "SparkBoot/BOOTX64.EFI" },
             { "/system/kernel.bin", "SparkKernel/kernel.bin" }
         };
 
         static int Main(string[] args)
         {
+            Directory.SetCurrentDirectory("../../../");
+            Console.WriteLine(Directory.GetCurrentDirectory());
             string target = args.Length > 0 ? args[0] : "x86_64-Debug";
             DirectoryInfo target_dir = new DirectoryInfo(string.Format("out/{0}", target));
 
@@ -39,8 +41,7 @@ namespace Bootstrap
             Console.WriteLine("==> Making directories...");
             Console.ResetColor();
 
-            Directory.CreateDirectory(string.Format("out/{0}/iso/system", target));
-            Directory.CreateDirectory(string.Format("out/{0}/boot/grub", target));
+            Directory.CreateDirectory(string.Format("out/{0}/system", target));
 
             Console.ForegroundColor = ConsoleColor.Magenta;
 
@@ -50,11 +51,15 @@ namespace Bootstrap
             foreach (string project_path in projects)
             {
                 DirectoryInfo project = new DirectoryInfo(string.Format("out/{0}/{1}", target, project_path));
+                if (!project.Exists)
+                    Directory.CreateDirectory(string.Format("out/{0}/{1}", target, project_path));
 
-                if (!Directory.Exists(project_path))
+                if (!Directory.Exists(project_path) || !File.Exists(string.Format("{0}/CMakeLists.txt", project_path)))
                 {
                     Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(string.Format("==> Project '{0}' does not exist. <==", project_path));
+
+                    Console.WriteLine(string.Format("CMake project '{0}' is missing CMakeLists.txt or does not exist. <==", project_path));
+                    Console.ResetColor();
                     return 1;
                 }
 
@@ -64,12 +69,59 @@ namespace Bootstrap
                 foreach (DirectoryInfo d in project.GetDirectories())
                     d.Delete(true);
 
-                Process.Start("cmake", "-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++");
+                Directory.SetCurrentDirectory(string.Format("out/{0}/{1}", target, project_path));
+
+                try
+                {
+                    ProcessStartInfo cmake = new ProcessStartInfo("cmake.exe", string.Format("-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++ -DCMAKE_ASM_NASM_COMPILER=nasm -G \"Ninja\" ../../../{0}", project_path));
+                    Process cmake_p = Process.Start(cmake);
+
+                    cmake_p.WaitForExit();
+
+                    if (cmake_p.ExitCode != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine(string.Format("CMake exited with exit code {0}", cmake_p.ExitCode));
+                        Console.ResetColor();
+
+                        return 1;
+                    }
+
+
+                    ProcessStartInfo ninja = new ProcessStartInfo("ninja.exe");
+                    Process ninja_p = Process.Start(ninja);
+
+                    ninja_p.WaitForExit();
+
+                    if (ninja_p.ExitCode != 0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+
+                        Console.WriteLine(string.Format("Ninja exited with exit code {0}", ninja_p.ExitCode));
+                        Console.ResetColor();
+
+                        return 1;
+                    }
+
+                }
+                catch (System.ComponentModel.Win32Exception exc)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+
+                    Console.WriteLine(string.Format("An error occurred:\n\r{0}", exc.Message));
+                    Console.ResetColor();
+
+                    return 1;
+                }
+
+                Directory.SetCurrentDirectory("../../");
             }
 
             Console.ForegroundColor = ConsoleColor.Blue;
 
             Console.WriteLine("==> Bundling image...");
+            Console.ResetColor();
 
             SetupHelper.SetupComplete();
 
@@ -86,6 +138,7 @@ namespace Bootstrap
             Console.ForegroundColor = ConsoleColor.Green;
 
             Console.WriteLine("==> Finished operation.");
+            Console.ResetColor();
             return 0;
         }
     }
